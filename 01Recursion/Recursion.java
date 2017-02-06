5c373a99f934e71855f3dbc74e423b5ef1e0d76e
DesperadoSP <khazix595@yahoo.com>
2017-02-06 18:17:18 -0500

    Potentially got sqrt function working

---
+++ 01Recursion/Recursion.java
@@ -0,0 +1,41 @@
+public class Recursion{
+    public static String name(){
+	return "Zhu, Edwin";
+    }
+    public static double sqrt(double n){
+	if (n == 0){
+	    return 0.0;
+	}else{
+	return helper(n, 1);
+	}
+    }
+
+    public static double helper(double z, double guess){
+	if (close((guess * guess), z)){
+	    return (double)Math.round(guess * 100) / 100;
+	}
+	double a = (z / guess + guess) / 2;
+        return helper(z, a);
+	
+
+
+    }
+
+    public static boolean close(double a, double b){
+	if ((Math.abs(((a - b) / b)) * 100) <= 0.00000001){
+	    return true;
+	}else{
+	    return false;
+	}
+    }
+    public static void main(String[]args){
+        System.out.println(sqrt(0.05));
+        
+        
+    }
+
+
+
+
+}
+
