923fe163033006db3eb904283e259f7b2552349b
Edwin <khazix595@yahoo.com>
2017-03-21 12:27:53 -0400

    whyyyyy

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,23 +1,25 @@
 import java.util.*;
 public class Quick{
 
+
+    public static void quicksort(int[] data){
+
+
+
+    }
     public static int quickselect(int[]data, int k){
-	int i = 0;
-	while (i == 0){
-	    if(part(data, 0, data.length - 1) == k){
-		return data[k];
-	    }
-	}
-	return 0;
+       
 
 	
 
     }
+    
 
     public static int part(int[]data, int start, int end){
 	Random a = new Random();
         int pivotIndex = start + a.nextInt(end - start + 1);
 	int pivot = data[pivotIndex];
+
 	for (int i = start; i <= end; i++){
 	    if (((i >= pivotIndex) && (data[i] <= pivot)) || ((i <= pivotIndex) && (data[i] >= pivot))){
 		int store = data[i];
@@ -31,9 +33,14 @@ public class Quick{
 
     }
 
+    public static void swap(int[] data, int one, int two){
+	int var = data[one];
+	data[one] = data[two];
+	data[two] = var;
+    }
+
     public static void main(String[]args){
-	int[] z = {6, 3, 10, 2, 8, 1, 9};
-        System.out.println(quickselect(z, 6));
+
     }
 
 }
