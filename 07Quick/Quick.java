813a5936de42b8aa50e3d85627650cfcab5916d7
DesperadoSP <khazix595@yahoo.com>
2017-03-21 20:10:30 -0400

    ah yes

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -3,34 +3,40 @@ public class Quick{
 
 
     public static void quicksort(int[] data){
-
+	part(data, 0, data.length - 1);
 
 
     }
     public static int quickselect(int[]data, int k){
-       
+        quicksort(data);
+	return data[k];
 
 	
 
     }
     
 
-    public static int part(int[]data, int start, int end){
-	Random a = new Random();
-        int pivotIndex = start + a.nextInt(end - start + 1);
-	int pivot = data[pivotIndex];
-
-	for (int i = start; i <= end; i++){
-	    if (((i >= pivotIndex) && (data[i] <= pivot)) || ((i <= pivotIndex) && (data[i] >= pivot))){
-		int store = data[i];
-		data[i] = pivot;
-		data[pivotIndex] = store;
-		pivotIndex = i;
+    public static void part(int[]data, int start, int end){
+        if (end <= start){
+	    return;
+	}
+	int lt = start;
+	int gt = end;
+	int v = data[start];
+	int i = start;
+	while (i <= gt){
+	    if (data[i] < v){
+		swap(data, lt++, i++);
+	    }
+	    else if(data[i] > v){
+		swap(data, i, gt--);
+	    }
+	    else{
+		i++;
 	    }
 	}
-	return pivotIndex;
-
-
+	part(data, start, lt - 1);
+	part(data, gt + 1, end);
     }
 
     public static void swap(int[] data, int one, int two){
@@ -40,7 +46,12 @@ public class Quick{
     }
 
     public static void main(String[]args){
-
+	int[]a = {5, 5, 5, 5, 1, 5, 8, 2, 10, 16, 39, 0};
+	quicksort(a);
+	for (int i = 0; i < a.length; i++){
+	    System.out.println(a[i]);
+	}
+	System.out.println(quickselect(a, 7));
     }
 
 }
