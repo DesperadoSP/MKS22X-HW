dbca4d073614e8009309893f204f86732a0facab
Edwin <khazix595@yahoo.com>
2017-03-28 12:27:42 -0400

    nuu

---
+++ 09MyLinkedList/MyLinkedList.java
@@ -0,0 +1,35 @@
+public class MyLinkedList{
+    private LNode start;
+    private int size; 
+
+    private static class LNode{
+
+
+	public LNode(int value){
+ 
+	}
+
+	public LNode(int[]
+
+
+   }
+
+    public MyLinkedList(){
+
+    }
+ 
+    public boolean add(int value){
+
+    }
+
+    public int size(){
+
+    }
+
+    public String toString(){
+
+
+    }
+
+   
+}
