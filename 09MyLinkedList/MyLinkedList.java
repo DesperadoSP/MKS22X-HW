55691da1b42fe94fb20cca77eced97d5133ef09f
DesperadoSP <khazix595@yahoo.com>
2017-03-28 20:44:56 -0400

    uh

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -1,35 +1,35 @@
 public class MyLinkedList{
     private LNode start;
-    private int size; 
+    private int size;
 
     private static class LNode{
-
+	private int data;
+	private LNode next;
 
 	public LNode(int value){
- 
+	    data = value;
 	}
 
-	public LNode(int[]
-
-
-   }
+	public LNode(int value, LNode nex){
+	    next = nex;
+	}
+    }
 
     public MyLinkedList(){
 
     }
- 
-    public boolean add(int value){
 
+	
     }
-
-    public int size(){
-
-    }
-
-    public String toString(){
+	
 
 
-    }
 
-   
+
+
+
+
+
+
+
 }
