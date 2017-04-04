3e5586bdf901e8559bf852d21bccea4e86675adc
Edwin <khazix595@yahoo.com>
2017-04-04 11:28:21 -0400

    errrrr

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -1,12 +1,12 @@
 public class MyLinkedList{
-    private LNode start;
+    private LNode start, end;
     private int size;
 
     private static class LNode{
 	private int data;
-	private LNode next;
-
-	public LNode(int value){
+	private LNode next, prev;
+	
+        public LNode(int value){
 	    data = value;
 	}
 
@@ -16,11 +16,80 @@ public class MyLinkedList{
     }
 
     public MyLinkedList(){
+	size = 0;
+	end = new LNode(null);
+	start = new LNode(null, end);
+    }
+
+    public boolean add(int value){
+
+
+
+    }
+
+    public int size(){
+	return size;
+    }
+
+    public String toString(){
+	int noob = size();
+	LNode copy = start;
+	String a = "[";
+	while (noob > 0){
+	    if (noob == 1){
+		a += ""+copy.data+"]";
+	    }else{
+                a += ""+copy.data+", ";
+	    }
+	    copy = copy.next;
+	    noob--;
+	}
+	System.out.println(a);
+    }
+
+    
+    public int get(int index){
+	try{
+	int a = index;
+	Lnode current = start;
+	while (a > 0){
+	    current = current.next;
+	    a--;
+	}
+	return current.data;
+	}
+	catch (IndexOutOfBoundsException e){
+	    System.out.println("Invalid index");
+	}
+    }
+
+    public int set(int index, int newValue){
+
+    }
+
+    public int indexOf(int value){
+	Lnode current = start;
+	int size1 = 0;
+	while (size1 < size()){
+	    if (current.data == value){
+		return size1;
+	    }
+            current = current.next;
+	    size1++;
+	}
+	return -1;
+    }
+
+    public void add(int index, int value){
+
+    }
+
+    public int remove(int index){
 
     }
 
 	
-    }
+ }
 	
 
 
@@ -32,4 +101,4 @@ public class MyLinkedList{
 
 
 
-}
+
