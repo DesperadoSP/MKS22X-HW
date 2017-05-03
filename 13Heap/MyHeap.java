3e13b9ba8a21f32ae9942d97dd27ca4f437b7e29
Edwin <khazix595@yahoo.com>
2017-05-03 11:33:04 -0400

    derp

---
+++ 13Heap/MyHeap.java
@@ -0,0 +1,20 @@
+import.java.util.*;
+public class MyHeap{
+    private ArrayList<String> a;
+    private int size;
+    private boolean max;
+    
+    public MyHeap(boolean a){
+	if (a){
+	    max = true;
+	}else{
+	    max = false;
+	}
+	a = new ArrayList<String>(10);
+    }
+    
+    public void add(String x){
+
+    }
+
+public void 
