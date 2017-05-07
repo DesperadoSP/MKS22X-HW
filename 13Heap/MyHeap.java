4388d454ad9bc31a66d259c786deed5b7b035f3a
DesperadoSP <khazix595@yahoo.com>
2017-05-07 12:40:45 -0400

    updated remove to return string

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -27,7 +27,7 @@ public class MyHeap{
 	a.set(0, ""+size+"");
     }
 
-    public void remove(){
+    public String remove(){
 	ArrayList<String> sub = a;
 	a = new ArrayList<String>(sub.size() + 5);
 	size = 0;
@@ -35,8 +35,9 @@ public class MyHeap{
 	for (int i = 2; i < sub.size(); i++){
 	    this.add(sub.get(i));
 	}
-	
+	return sub.get(1);
     }
+    
     private void pushUp(){
 	if (max){
 	int index = a.size() - 1;
@@ -84,7 +85,7 @@ public class MyHeap{
 	q.add("8");
 	q.add("5");
 	q.add("2");
-	q.remove();
+        System.out.println(q.remove());
         System.out.println(q);
     }
 }
